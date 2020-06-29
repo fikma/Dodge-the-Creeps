760d378cfbe58be15f2934dc0ff82fd5cfe5129b
Ade Fikri Malihuddin <ade.fm97@gmail.com>
2020-06-29 09:52:39 +0700

    Enemy Script

---
+++ Enemy.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+
+public class Enemy : RigidBody2D
+{
+	[Export]
+	private int _minSpeed = 150;
+
+	[Export]
+	private int _maxSpeed = 250;
+
+	private string[] _mobTypes = { "walk", "swim", "fly" };
+
+	public override void _Ready()
+	{
+		GetNode<AnimatedSprite>("AnimatedSprite").Animation =
+			_mobTypes[GD.Randi() % _mobTypes.Length];
+
+	}
+
+    private void _on_Visibility_screen_exited()
+    {
+        QueueFree();
+    }
+}
